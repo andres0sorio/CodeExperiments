7255f6930182845db0bdcafe38d40b9905b1ee8b
andres0sorio <osorio.af@gmail.com>
2017-06-23 10:41:14 -0500

    [Config] Added port configuration from environment

--- fichedbsvc/src/main/java/co/phystech/aosorio/app/Main.java
+++ fichedbsvc/src/main/java/co/phystech/aosorio/app/Main.java
@@ -5,6 +5,7 @@ package co.phystech.aosorio.app;
 
 import static spark.Spark.get;
 import static spark.Spark.options;
+import static spark.Spark.port;
 import static spark.Spark.post;
 
 import co.phystech.aosorio.controllers.BookController;
@@ -25,27 +26,29 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
+
+		port(getHerokuAssignedPort());
+
 		CorsFilter.apply();
-		
+
 		post(Routes.FICHES, FicheController::createFiche, GeneralSvc.json());
 
 		get(Routes.FICHES, FicheController::readFiches, GeneralSvc.json());
-		
+
 		post(Routes.BOOKS, BookController::createBook, GeneralSvc.json());
 
 		get(Routes.BOOKS, BookController::readBooks, GeneralSvc.json());
-		
+
 		post(Routes.COMMENTS, CommentController::createComment, GeneralSvc.json());
 
 		get(Routes.COMMENTS, CommentController::readComments, GeneralSvc.json());
-		
+
 		get("/statistics", StatisticsSvc::getBasicStats, GeneralSvc.json());
-		
-		//get("/fiches/:uuid")		
-		//post("/posts/:uuid/comments");		
-		//get("/posts/:uuid/comments");
-	
+
+		// get("/fiches/:uuid")
+		// post("/posts/:uuid/comments");
+		// get("/posts/:uuid/comments");
+
 		options("/*", (request, response) -> {
 
 			String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
@@ -58,7 +61,15 @@ public class Main {
 			}
 			return "OK";
 		});
-		
+
+	}
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567;
 	}
 
 }
